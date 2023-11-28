026a2eab55776c10fb25c2dff48be1ff2c25a934
Wesley Araujo <wesleyjk85@gmail.com>
2023-11-28 20:22:07 -0300

    Atualização dos codigos

--- src/app/app.java
+++ src/app/app.java
@@ -1,34 +1,46 @@
 package app;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class app {
 
 	public static void main(String[] args) {
 		
-		File file = new File("C:\\temp\\ws-eclipse-udemy\\curso\\Arquivos tst\\tabelaPreco.txt");
-		Scanner sc = null;
+		String path = "C:\\temp\\ws-eclipse-udemy\\curso\\Arquivos tst\\tabelaPreco.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 		
 		try {
-			sc = new Scanner (file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();	
 			}
-			
 		}
-		catch(IOException e){
-			System.out.println("Error n planejado: " + e.getMessage());
-			
+		catch(IOException e) {
+			System.out.println("Erro meu: " + e.getMessage());
 		}
-		finally{
-			if (sc != null) {
-				sc.close();
+		finally {
+			
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+				if (br != null) {
+					br.close();
+				}
+			}
+			catch(IOException e){
+				e.getStackTrace();
 			}
 		}
 		
-
 	}
 
 }
